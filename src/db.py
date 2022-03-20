4554bad0a590d783bc2b5dff5f950167d93d9264
Laureηt <laurentfainsin@protonmail.com>
2022-03-20 17:38:43 +0100

    feat: testing dataset augmentation

--- src/db.py
+++ src/db.py
@@ -1,6 +1,8 @@
+import io
 import sqlite3
 import uuid
-from fileinput import filename
+
+from PIL import Image, ImageOps
 
 DATA_FOLDER = "data/"
 
@@ -13,9 +15,18 @@ device = client.device("13231FDD4003UE")
 
 filename = uuid.uuid1()
 result = device.screencap()
-with open(f"{DATA_FOLDER}/{filename}.png", "wb") as fp:
-    fp.write(result)
-    con.execute(f"INSERT INTO data VALUES ('{filename}',{0},{0},{0.5},{0.5},{0},{0},{0},{0},{0})")
-    con.commit()
+
+image = Image.open(io.BytesIO(result))
+image = image.crop((0, 160, 2200, 860))
+image = image.resize((150, 150), Image.ANTIALIAS)
+
+image.save("data/test.png")
+image.rotate(90).save("data/test90.png")
+image.rotate(180).save("data/test180.png")
+image.rotate(-90).save("data/test-90.png")
+ImageOps.mirror(image).save("data/testmirror.png")
+
+con.execute(f"INSERT INTO data VALUES ('{filename}',{0},{0},{0.5},{0.5},{0},{0},{0},{0},{0})")
+con.commit()
 
 con.close()
