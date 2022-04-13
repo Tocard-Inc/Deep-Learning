3b76c8e22219a7d27242b6f46739c1cd2aa3afdb
gdamms <damguillotin@gmail.com>
2022-04-13 09:28:31 +0200

    path dans les variables

--- src/live.py
+++ src/live.py
@@ -13,25 +13,24 @@ import utils
 RESIZED_SIZE = (100, 50, 3)
 LABELS = ['octane', 'werewolf', 'breakout', 'aftershock']
 
+MODELS_PATH = '../models'
+MODEL_FILENAME = 'rot_25e'
+
 # Load model
-model = models.load_model('../models/model')
+model = models.load_model(MODELS_PATH + '/' + MODEL_FILENAME)
 
 utils.startup(need_focus=False)
 
 running = True
 
 while running:
-    utils.screenshot(filename='live', folder='../models')
+    utils.screenshot(filename='live', folder=MODELS_PATH)
     
     # Lecture de l'image
-    img = PIL.Image.open('../models/live')
-
-    # print(f"img.size = {img.size}")
+    img = PIL.Image.open(MODELS_PATH + '/live')
 
     # Redimensionnement de l'image
     img = img.resize(RESIZED_SIZE[:-1], PIL.Image.ANTIALIAS)
-
-    # print(f"img.size = {img.size}")
 
     X = np.asarray(img)
 
